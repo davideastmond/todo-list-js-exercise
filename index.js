b195e641922cf0fb36e5d3da44a7231db205dc51
David Eastmond <davebuck1981@gmail.com>
2019-05-29 18:14:45 +0000

    modified code so driver functions access the new object

--- index.js
+++ index.js
@@ -9,23 +9,26 @@ function newTask(title, description) {
   return task;
 }
 
+
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
+  task.complete = true;
 }
+
 
 // Print the state of a task to the console in a nice readable way
 function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+const task2 = newTask("Do laundry", "dirty clothes");
+const allTasks = [task1, task2];
 
+console.log(allTasks);
+/*
 logTaskState(0); // Clean Cat Litter has not been completed
 completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+logTaskState(0); // Clean Cat Litter has been completed */
